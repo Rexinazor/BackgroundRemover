f75d7465ab09436caff8399e6495fbc807b6fbca
Ｒｅｘｉｎａｚｏｒ <85327329+Rexinazor@users.noreply.github.com>
2021-08-12 13:47:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# Remove-BG-Bot
+
+A photo background remover telegram bot with pyrogram framework.
+
+---
+
+```
+Made with Python3
+(C) @shrvan42
+Copyright permission under MIT License
+License -> https://github.com/shrvan42/Remove-BG-Bot/blob/main/LICENSE
+```
+
+---
+
+## Deploy
+
+<details>
+  <summary><b>Deploy on Heroku</b></summary>
+
+<p align="left">
+  <a href="https://heroku.com/deploy?template=https://github.com/shrvan42/Remove-BG-Bot">
+     <img height="30px" src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku">
+  </a>
+</p>
+
+</details>
+
+<details>
+  <summary><b>Deploy in your vps</b></summary>
+
+## Variables
+
+<details>
+  <summary><b>See variables</b></summary>
+
+- `API_HASH` Your API Hash from my.telegram.org
+- `API_ID` Your API ID from my.telegram.org
+- `BOT_TOKEN` Your bot token from @BotFather
+- `REMOVEBG_API` Your remove bg api from removebg.com/api
+
---
+++ main.py
@@ -0,0 +1,172 @@
+# Made with python3
+# (C) @shrvan42
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/shrvan42/Remove-BG-Bot/blob/main/LICENSE
+
+import os
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+API = os.environ["REMOVEBG_API"]
+IMG_PATH = "./DOWNLOADS"
+
+Shrvan42 = Client(
+    "Remove Background Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+)
+
+START_TEXT = """
+Hello {}, I am a photo background remover bot. Send me a photo I will send the photo without background.
+
+Made by [TgxBotz](t.me/tgxbotz_chat).
+"""
+HELP_TEXT = """
+- Just send me a photo
+- I will download it
+- I will send the photo without background
+
+Made by [TgxBotz](t.me/tgxbotz_chat).
+"""
+ABOUT_TEXT = """
+- **Bot :** `Backround Remover Bot`
+- **Creator :** [SHRVAN](https://t.me/SHRVAN42)
+- **Channel :** [TGxBOTZ](https://t.me/tgxbotz_update)
+- **Source :** [Here](https://github.com/shrvan42/Remove-BG-Bot/tree/main)
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Channel🇮🇳', url='https://telegram.me/tgxbotz_update'),
+        InlineKeyboardButton('Repo🇮🇳', url='https://github.com/shrvan42/Remove-BG-Bot')
+        ],[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ERROR_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Join Updates Channel', url='https://t.me/tgxbotz_update')
+        ]]
+    )
+
+@Shrvan42.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            reply_markup=HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            reply_markup=ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
+
+@Shrvan42.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTONS
+    )
+
+@Shrvan42.on_message(filters.private & (filters.photo | filters.document))
+async def remove_background(bot, update):
+    if not API:
+        await update.reply_text(
+            text="Error :- Remove BG Api is error",
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=ERROR_BUTTONS
+        )
+        return
+    await update.reply_chat_action("typing")
+    message = await update.reply_text(
+        text="Analysing",
+        quote=True,
+        disable_web_page_preview=True
+    )
+    if (update and update.media and (update.photo or (update.document and "image" in update.document.mime_type))):
+        file_name = IMG_PATH + "/" + str(update.from_user.id) + "/" + "image.jpg"
+        new_file_name = IMG_PATH + "/" + str(update.from_user.id) + "/" + "no_bg.png"
+        await update.download(file_name)
+        await message.edit_text(
+            text="Photo downloaded successfully. Now removing background.",
+            disable_web_page_preview=True
+        )
+        try:
+            new_image = requests.post(
+                "https://api.remove.bg/v1.0/removebg",
+                files={"image_file": open(file_name, "rb")},
+                data={"size": "auto"},
+                headers={"X-Api-Key": API}
+            )
+            if new_image.status_code == 200:
+                with open(f"{new_file_name}", "wb") as image:
+                    image.write(new_image.content)
+            else:
+                await update.reply_text(
+                    text="API is error.",
+                    quote=True,
+                    reply_markup=ERROR_BUTTONS
+                )
+                return
+            await update.reply_chat_action("upload_photo")
+            await update.reply_document(
+                document=new_file_name,
+                quote=True
+            )
+            await message.delete()
+            try:
+                os.remove(file_name)
+            except:
+                pass
+        except Exception as error:
+            print(error)
+            await message.edit_text(
+                text="Something went wrong! May be API limits.",
+                disable_web_page_preview=True,
+                reply_markup=ERROR_BUTTONS
+            )
+    else:
+        await message.edit_text(
+            text="Media not supported",
+            disable_web_page_preview=True,
+            reply_markup=ERROR_BUTTONS
+        )
+
+Shrvan42.run()
